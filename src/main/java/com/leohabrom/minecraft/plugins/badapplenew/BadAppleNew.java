925f4e49752934510ad23ad7a56278bfa17f463b
tze62 <leonard.habrom@uni-ulm.de>
2024-08-15 10:40:58 +0200

    Initial commit

---
+++ src/main/java/com/leohabrom/minecraft/plugins/badapplenew/BadAppleNew.java
@@ -0,0 +1,17 @@
+package com.leohabrom.minecraft.plugins.badapplenew;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BadAppleNew extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
